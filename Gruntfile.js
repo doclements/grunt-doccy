4c2223e33e28b34169dfb8983c5abaf905ad1bea
doclements <d.o.clements@googlemail.com>
2013-11-20 10:47:52 +0000

    hopefully fixing this saga

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,74 +1,3 @@
-<<<<<<< HEAD
-/*
- * grunt-doccy
- * https://github.com/olcl/grunt-doccy
- *
- * Copyright (c) 2013 doclements
- * Licensed under the MIT license.
- */
-
-'use strict';
-
-module.exports = function(grunt) {
-
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>',
-      ],
-      options: {
-        jshintrc: '.jshintrc',
-      },
-    },
-
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp'],
-    },
-
-    // Configuration to be run (and then tested).
-    doccy: {
-      default_options: {
-        options: {
-          src_dir : 'test/src',
-          out_dir : 'test/docs'
-        }
-      },
-      custom_options: {
-        options: {
-          src_dir : 'test/src',
-          out_dir : 'test/docs'
-        }
-      },
-    },
-
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js'],
-    },
-
-  });
-
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
-
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'doccy', 'nodeunit']);
-
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
-
-};
-=======
 /*
  * grunt-doccy
  * https://github.com/doclements/grunt-doccy
@@ -142,4 +71,3 @@ module.exports = function(grunt) {
   grunt.registerTask('default', ['jshint', 'test']);
 
 };
->>>>>>> a51cf10b7fe94196d86cae358418220c507fb070
--- test/doccy_test.js
+++ test/doccy_test.js
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-'use strict';
-
-var grunt = require('grunt');
-
-/*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
-
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
-*/
-
-exports.doccy = {
-  setUp: function(done) {
-    // setup here if necessary
-    done();
-  },
-  default_options: function(test) {
-    test.expect(1);
-
-    //var actual = grunt.file.read('tmp/default_options');
-    //var expected = grunt.file.read('test/expected/default_options');
-    test.equal(1, 1, 'should describe what the default behavior is.');
-
-    test.done();
-  },
-  custom_options: function(test) {
-    test.expect(1);
-
-    // var actual = grunt.file.read('tmp/custom_options');
-    // var expected = grunt.file.read('test/expected/custom_options');
-     test.equal(1, 1, 'should describe what the custom option(s) behavior is.');
-    test.done();
-  },
-};
-=======
 'use strict';
 
 var grunt = require('grunt');
@@ -95,4 +46,3 @@ exports.doccy = {
     test.done();
   },
 };
->>>>>>> a51cf10b7fe94196d86cae358418220c507fb070
